729ee22c8310d103b8a67171cd668175af21b815
GABRIELA <palermogabrielaribeiro@hotmail.com>
2020-10-24 11:43:01 -0300

    Created register screen

---
+++ app/src/main/java/wcc/com/lanadordedados/Registro_activity.kt
@@ -0,0 +1,11 @@
+package wcc.com.lanadordedados
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Registro_activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro_activity)
+    }
+}
